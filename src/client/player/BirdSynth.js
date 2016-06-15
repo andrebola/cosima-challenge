73882b36556ae73f4e71c56f6e2001ef87928a8e
Norbert Schnell <Norbert.Schnell@ircam.fr>
2016-06-15 17:35:33 +0200

    added bird sounds and synth

---
+++ src/client/player/BirdSynth.js
@@ -0,0 +1,67 @@
+import { audio, audioContext } from 'soundworks/client';
+
+function getNearestIndexByValue(sortedArray, value, randomVar) {
+  var size = sortedArray.length;
+  var index = 0;
+
+  if (size > 0) {
+    var firstVal = sortedArray[randomVar].value;
+    var lastVal = sortedArray[size - 1 - randomVar].value;
+
+    if (value <= firstVal)
+      index = randomVar;
+    else if (value >= lastVal)
+      index = size - 1 - randomVar;
+    else {
+      if (index < 0 || index >= size)
+        index = Math.floor((size - 1) * (value - firstVal) / (lastVal - firstVal) + 0.5);
+
+      while (sortedArray[index].value > value)
+        index--;
+
+      while (sortedArray[index + 1].value <= value)
+        index++;
+
+      if ((value - sortedArray[index].value) >= (sortedArray[index + 1].value - value))
+        index++;
+    }
+  }
+
+  index += (Math.floor(2 * randomVar * Math.random()) - randomVar);
+
+  return sortedArray[index].index;
+}
+
+export default class BirdSynth {
+  constructor(output, audioBuffer, markerArray) {
+    this.engine = new audio.SegmentEngine();
+
+    this.engine.periodAbs = 0;
+    this.engine.periodRel = 0.5;
+    this.engine.durationAbs = 0;
+    this.engine.durationRel = 1;
+    this.engine.offset = 0;
+    this.engine.attackAbs = 0.005;
+    this.engine.attackRel = 0.0;
+    this.engine.releaseAbs = 0.0;
+    this.engine.releaseRel = 0.25;
+    this.engine.resamplingVar = 100;
+    this.engine.gain = 1.0;
+
+    this.engine.buffer = audioBuffer;
+    this.engine.positionArray = markerArray.time;
+    this.engine.durationArray = markerArray.duration;
+
+    this.engine.connect(output);
+
+    this.sortedMarkerIndices = markerArray.energy.map((value, index) => {
+      return { value, index };
+    }).sort((a, b) => a.value - b.value);
+  }
+
+  trigger(energy) {
+    this.engine.segmentIndex = getNearestIndexByValue(this.sortedMarkerIndices, energy, 3);
+    this.engine.gain = energy * energy;
+    this.engine.trigger();
+  }
+}
--- src/client/player/PlayerExperience.js
+++ src/client/player/PlayerExperience.js
@@ -1,5 +1,6 @@
 import * as soundworks from 'soundworks/client';
 import PlayerRenderer from './PlayerRenderer';
+import BirdSynth from './BirdSynth';
 
 const View = soundworks.View;
 const audioContext = soundworks.audioContext;
@@ -12,24 +13,33 @@ const viewTemplate = `
   </div>
   <div class="section-bottom"></div>
 `;
+
+const birdNames = ['alauda', 'larus', 'picus', 'turdus'];
+
 // this experience plays a sound when it starts, and plays another sound when
 // other clients join the experience
 export default class PlayerExperience extends soundworks.Experience {
-  constructor(assetsDomain, audioFiles) {
+  constructor(assetsDomain, files) {
     super();
 
     this.platform = this.require('platform', { showDialog: true });
 
+    this.loader = this.require('loader', { files });
+
     this.onStartMessage = this.onStartMessage.bind(this);
     this.onStopMessage = this.onStopMessage.bind(this);
     this.wait = true;
-
   }
 
   init() {
     this.viewTemplate = viewTemplate;
     this.viewCtor = View;
     this.view = this.createView();
+
+    const output = audioContext.destination;
+    const birdIndex = Math.floor(birdNames.length * Math.random());
+    const { audio, markers } = this.loader.get(birdNames[birdIndex]);
+    this.birdSynth = new BirdSynth(output, audio, markers);
   }
 
   start() {
@@ -39,10 +49,13 @@ export default class PlayerExperience extends soundworks.Experience {
       this.init();
 
     this.show();
-    var that = this;
+
     // If the user clicks the button the send notification to server
-    document.getElementById("button").addEventListener("click", function(){that.onTouchEnd()});
-   
+    document.getElementById('button').addEventListener('click', () => {
+      const energy = Math.random();
+      this.birdSynth.trigger(energy);
+    });
+
     // When server send stop and start message execute corresponding functions
     this.receive('start', this.onStartMessage);
     this.receive('stop', this.onStopMessage);
